272c6c5633d9417769383f96f1e58adcdc658d11
trailynx <trailynx@outlook.com>
2013-04-13 20:30:24 +0200

    added onpinch, onpinchdelta, onpinchcompleted and ondoubletap handler

--- TinyGame/TinyGame/MainPage.xaml.cs
+++ TinyGame/TinyGame/MainPage.xaml.cs
@@ -10,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 using Microsoft.Phone.Controls;
+using TinyGame.Utilities;
 
 namespace TinyGame
 {
@@ -19,7 +20,36 @@ namespace TinyGame
         public MainPage()
         {
             InitializeComponent();
-            rctMain.Height = rctMain.Width;
+        }
+
+        private void OnPinchStarted(object sender, PinchStartedGestureEventArgs e)
+        {
+            ImageZoomHelper.OnPinchStarted(sender, e);
+        }
+
+        private void OnPinchDelta(object sender, PinchGestureEventArgs e)
+        {
+            ImageZoomHelper.OnPinchDelta(sender, e);
+        }
+
+        private void OnDragDelta(object sender, DragDeltaGestureEventArgs e)
+        {
+            if (((CompositeTransform)((Image)sender).RenderTransform).ScaleX <= 1.1)
+            {
+                return;
+            }
+            ImageZoomHelper.OnDragDelta(sender, e);
+        }
+
+       
+        private void OnDoubleTap(object sender, Microsoft.Phone.Controls.GestureEventArgs e)
+        {
+            ImageZoomHelper.OnDoubleTap(sender, e);
+        }
+
+        private void OnPinchCompleted(object sender, PinchGestureEventArgs e)
+        {
+            MessageBox.Show(String.Format("Height={0}, \n Width={1}", ((Image)sender).ActualHeight, ((Image)sender).ActualWidth));
         }
     }
 }
