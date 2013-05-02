f392e555ef44c431f7b53b453437bf05eb7cc855
trailynx <trailynx@outlook.com>
2013-05-02 15:39:21 +0200

    added Model.ScoreEntry

---
+++ TinyGame/TinyGame/Model/ScoreEntry.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace TinyGame.Model
+{
+    [DataContract]
+    public class ScoreEntry
+    {
+        [DataMember]
+        public int DisplayScore { get; set; }
+        [DataMember]
+        public double Score { get; set; }
+        [DataMember]
+        public string PlayerName { get; set; }
+    }
+}
